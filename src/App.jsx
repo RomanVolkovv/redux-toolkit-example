4eeac9227f379f2b1f4b3058da2a1531d374eaba
RomanVolkovv <104730445+RomanVolkovv@users.noreply.github.com>
2022-07-20 14:21:53 +0300

    redux-tollkit-todo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./store/todoSlice";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
